36e21eaad4fb37a89c14dc43a6848f1f71acea0e
baharerol <baharerol.79@gmail.com>
2023-11-30 13:05:31 +0300

    Add project files

---
+++ Week_2_2/Program.cs
@@ -0,0 +1,32 @@
+﻿using Week_2_2.Entities;
+using Week_2_2.Services;
+
+Console.WriteLine("Sqotify - Music for Noone");
+
+string cuurentDirectory = Directory.GetParent(Directory.GetCurrentDirectory()).Parent?.Parent?.FullName;
+
+Console.WriteLine();
+
+var song1 = new Song("Requiem k. 626", "W. A. Mozart", "Orchestre national de France", null);
+var song2 = new Song("Sonata No.6 in F Major", "L. Beethoven", "Fazıl Say", "Warner Music");
+var song3 = new Song("Moonlight Sonata", "Ludwig van Beethoven", "Various Artists", "Classical Records");
+var song4 = new Song("The Four Seasons", "Antonio Vivaldi", "Chamber Orchestra", "Baroque Productions");
+
+
+
+var playlist1 = new Playlist(song1);
+playlist1.AddSong(song2);
+playlist1.AddSong(song3);
+playlist1.AddSong(song4);
+
+Console.WriteLine("Before Shuffle:");
+Console.WriteLine(playlist1.GetSongs());
+
+playlist1.ShuffleSongs();
+
+Console.WriteLine("\n\nAfter Shuffle:");
+Console.WriteLine(playlist1.GetSongs());
+
+NotepadServcie notepadService = new();
+notepadService.PlaylistToNotepad(playlist1);
+
---
+++ Week_2_2/Services/NotepadServcie.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Week_2_2.Entities;
+
+namespace Week_2_2.Services
+{
+    internal class NotepadServcie
+    {
+        public void PlaylistToNotepad(Playlist playlist)
+        {
+            string directory = $@"{Directory.GetParent(Directory.GetCurrentDirectory
+                ()).Parent?.Parent?.FullName}\Playlists";
+
+            if (!File.Exists(directory))
+                Directory.CreateDirectory(directory);
+
+            string saveFile = Path.Combine(directory, $"{playlist.Id}.txt");
+
+            File.WriteAllText(saveFile, playlist.GetSongs());
+
+            Console.WriteLine("Data sucessfully saved to Notepad!");
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+    }
+}
