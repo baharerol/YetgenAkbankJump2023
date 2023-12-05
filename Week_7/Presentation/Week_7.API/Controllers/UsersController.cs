136741ce4ba9c74dba9b982cb2fbc44b83ab3551
baharerol <baharerol.79@gmail.com>
2023-12-05 15:53:46 +0300

    Add project files

---
+++ Week_7/Presentation/Week_7.API/Controllers/UsersController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Week_7.API.Models;
+using Week_7.Domain.Entities;
+using Week_7.Persistence.Contexts;
+
+namespace Week_7.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        readonly Week_7DbContext _context;
+
+        public UsersController(Week_7DbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("[action]")]
+        public IActionResult GetAll()
+        {
+            return Ok(_context.Users.ToList());
+        }
+
+
+
+        [HttpPost("[action]")]
+
+        public IActionResult CreateUser([FromBody] CreateUserRequest createUserRequest)
+        {
+            User user = new()
+            {
+                FirstName = createUserRequest.FirstName,
+                LastName = createUserRequest.LastName,
+            };
+
+            _context.Users.Add(user);
+
+            //_context.SaveChanges();
+
+            return Ok();
+        }
+
+        [HttpGet("[action]")]
+        public string GetSomething(string name)
+        {
+            return name.Substring(0, 2);
+        }
+    }
+}
---
+++ Week_7/Presentation/Week_7.API/Program.cs
@@ -0,0 +1,29 @@
+using Week_7.Persistence.Contexts;
+using Week_7.Persistence.Contexts;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<Week_7DbContext, Week_7DbContext>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
